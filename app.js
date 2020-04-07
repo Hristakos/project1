ea974839987c57ab104a8459ea0e9eaa1787034a
Peter Hristakos <peter.hristakos@bigpond.com>
2020-04-08 09:33:03 +1000

    trying out some images for player1 and player2

--- app.js
+++ app.js
@@ -14,12 +14,12 @@ const winningCombinations = [
 
 let player1 = {
     selections: [],
-    symbol: "x"
+    backgroudImage: "url('images/Collingwood.png')"
 }
 
 let player2 = {
     selections: [],
-    symbol: "0"
+    backgroudImage: "url('images/Carlton.png')"
 }
 
 isPlayer1 = true;
@@ -35,19 +35,36 @@ let selectionAreas = document.querySelectorAll(".selection-area");
 let handleSelectionAreaClick = function (e) {
     attempts += 1;
     console.log(e.target.dataset.ref)
+    let winningCombination = [];
+    const boardPosition = Number(e.target.dataset.ref)
+
     if (isPlayer1) {
-        e.target.style.backgroundColor = "white";
-        player1.selections.push(Number(e.target.dataset.ref))
-        checkSelectionsForWinningCombination(player1.selections);
+        e.target.classList.replace("selection-area", "player-1");
+        e.target.style.backgroundImage = player1.backgroudImage;
+        player1.selections.push(boardPosition)
+        winningCombination = checkSelectionsForWinningCombination(player1.selections);
         isPlayer1 = false;
     } else {
-        e.target.style.backgroundColor = "green";
-        player2.selections.push(Number(e.target.dataset.ref))
-        checkSelectionsForWinningCombination(player2.selections);
+        e.target.classList.replace("selection-area", "player-2");
+        e.target.style.backgroundImage = player2.backgroudImage;
+        player2.selections.push(boardPosition)
+        winningCombination = checkSelectionsForWinningCombination(player2.selections);
         isPlayer1 = true;
     }
-    e.target.removeEventListener("click", handleSelectionAreaClick);
 
+    if (winningCombination) {
+        winningCombination.forEach(function (selection) {
+            console.log(selection);
+            selectionAreas[selection].classList = ["winner"];
+            //selectionAreas[selection].style.backgroundImage = "url('images/cup.jpeg')"
+
+            document.querySelectorAll(".selection-area").forEach(function (selectionArea) {
+                selectionArea.removeEventListener('click', handleSelectionAreaClick);
+            })
+        })
+
+    }
+    e.target.removeEventListener("click", handleSelectionAreaClick);
 }
 
 selectionAreas.forEach(function (selectionArea) {
@@ -67,10 +84,11 @@ let checkSelectionsForWinningCombination = function (playerSelections) {
     let matches = 0;
 
     if (playerSelections.length >= minSelections) {
+        let matchingCombination;
         let winningCombinationIndex = 0;
         // while no winning combination and haven't finished checking all winning cobinations
         while (matches < winningScore && winningCombinationIndex < winningCombinations.length) {
-
+            matchingCombination = [];
             let playerSelectionsIndex = 0;
             matches = 0;
             // console.log("1st while");
@@ -78,10 +96,11 @@ let checkSelectionsForWinningCombination = function (playerSelections) {
             while (matches < winningScore && playerSelectionsIndex < playerSelections.length) {
                 // console.log("2nd while")
                 let winningCombination = winningCombinations[winningCombinationIndex];
-
+                let playerSelection = playerSelections[playerSelectionsIndex]
                 // check if current selection is part of winning combination
-                if (winningCombination.includes(playerSelections[playerSelectionsIndex])) {
+                if (winningCombination.includes(playerSelection)) {
                     matches += 1;
+                    matchingCombination.push(playerSelection)
                 }
 
                 playerSelectionsIndex += 1;
@@ -94,8 +113,14 @@ let checkSelectionsForWinningCombination = function (playerSelections) {
         if (matches === winningScore) {
             isPlayer1 ? console.log("Winner is player 1!") :
                 console.log("Winner is player 2!")
+            return matchingCombination;
+
+        } else if (attempts === maxAttempts) {
+            console.log("We have a draw");
 
         }
+
+        return null;
 
     }
 
